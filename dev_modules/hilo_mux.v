041f3e255dd2a79453cfc270fe501c252ded7a98
Nopelands <rhas@cin.ufpe.br>
2021-07-20 01:26:21 -0300

    Fixed spelling and standardized hilo_mux

--- dev_modules/hilo_mux.v
+++ dev_modules/hilo_mux.v
@@ -1,12 +1,12 @@
-module hilomux (
+module hilo_mux (
 
     input wire  [31:0]  div, // outputs when true
-    input wire  [31:0]  mut, // outputs when false
+    input wire  [31:0]  mult, // outputs when false
     input wire          control,
 
     output wire [31:0]  data_out
 );
     
-    assign data_out = (control) ? div : mut;
+    assign data_out = (control) ? div : mult;
 
 endmodule
