134083c2b3bece9b6cff7698580ef8b3220df5fb
Nopelands <rhas@cin.ufpe.br>
2021-07-20 01:11:37 -0300

    Added control unit skeleton

---
+++ dev_modules/control_unit.v
@@ -0,0 +1,26 @@
+module control_unit (
+    input wire clk,
+    input wire reset,
+
+    //Instruction
+
+    //Flags
+
+    //Mux controllers
+
+    //Functional controllers
+);
+    
+    //State and cycle variables
+
+    //Parameters
+        //States
+        //Opcodes
+        //Other parameters
+
+    //Startup handler
+
+    //Transition function
+
+    //State output handler
+endmodule
