472fecf9fc185ce1e82c80244fd17e8bef3db9de
Nopelands <rhas@cin.ufpe.br>
2021-07-20 00:50:17 -0300

    Added mux responsible for HI and LO registers

---
+++ dev_modules/hilomux.v
@@ -0,0 +1,12 @@
+module hilomux (
+
+    input wire  [31:0]  div // outputs when true
+    input wire  [31:0]  mut // outputs when false
+    input wire          control
+
+    output wire [31:0]  data_out
+);
+    
+    assign data_out = (control) ? div : mut;
+
+endmodule
