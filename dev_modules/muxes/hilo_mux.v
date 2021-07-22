224008f3c72dc5c4998856bbfe60fe84546aec5d
Nopelands <rhas@cin.ufpe.br>
2021-07-22 06:30:43 -0300

    Standardized mux control position

--- dev_modules/muxes/hilo_mux.v
+++ dev_modules/muxes/hilo_mux.v
@@ -1,8 +1,8 @@
 module hilo_mux (
 
+    input wire          control,
     input wire  [31:0]  div, // outputs when true
     input wire  [31:0]  mult, // outputs when false
-    input wire          control,
 
     output wire [31:0]  data_out
 );
