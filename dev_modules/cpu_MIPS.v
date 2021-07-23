95a460078520fa0da7b0d8c74425429dfb0545b3
rrs6 <37545382+rrs6@users.noreply.github.com>
2021-07-23 10:32:43 -0300

    Update cpu_MIPS.v
    Only test

--- dev_modules/cpu_MIPS.v
+++ dev_modules/cpu_MIPS.v
@@ -4,376 +4,262 @@ module cpu_MIPS (
 );
 
 // Control wires
-
-    // Mux control wires
-    wire HI_mux_control;
-    wire LO_mux_control;
-    wire [1:0] in_mux_control;
-    wire [2:0] mem_mux_control;
-    wire [2:0] pc_mux_control;
-    wire [1:0] sa_mux_control;
-    wire [1:0] ula_a_mux_control;
-    wire [1:0] ula_b_mux_control;
-    wire [2:0] wd_mux_control;
-    wire [1:0] wr_mux_control;
-
-    // Register control wires
-    wire PC_control;
-    wire MDR_control;
-    wire A_control;
-    wire B_control;
-    wire HI_control;
-    wire LO_control;
-    wire ALUout_control;
-    wire EPC_control;
-
-    // Other control wires
-    wire [2:0] ula_control;
-    wire Register_bank_control;
-    wire Instruct_Reg_control;
-    wire MEM_control;
-    wire [2:0] Shift_register_control;
-
+    wire [2:0] PC_MUX;
+    wire [2:0] MEM_MUX;
+    wire [1:0] WR_MUX;
+    wire [2:0] WD_MUX;
+    wire [1:0] SA_MUX;
+    wire [1:0] IN_MUX;
+    wire [1:0] ULA_A_MUX;
+    wire [1:0] ULA_B_MUX;
+    wire [2:0] Shift;
+    wire [2:0] ALUOp;
+    wire PC_load;
+    wire MDR_load;
+    wire regA_load;
+    wire regB_load;
+    wire EPC_load;
+    wire ALUOut_load;
+    wire Mem_write;
+    wire IR_write;
+    wire REGS_write;
 // Data wires
-wire [31:0] ula_operand_A;
-wire [31:0] ula_operand_B;
-wire [31:0] ula_output;
-
-wire [31:0] PC_in;
-wire [31:0] PC_out;
-
-wire [31:0] Memory_data_out; // Memory output
-wire [31:0] MDR_out;
-
-wire [31:0] RegisterBank_out_A;
-wire [31:0] RegisterBank_out_B;
-wire [31:0] A_Register_out;
-wire [31:0] B_Register_out;
-
-wire [31:0] HI_mux_out;
-wire [31:0] LO_mux_out;
-wire [31:0] HI_Register_out;
-wire [31:0] LO_Register_out;
-
-wire [31:0] ALUout_output;
-
-wire [31:0] EPC_out;
-
-wire [5:0] OPcode;
-wire [4:0] RS;
-wire [4:0] RT;
-wire [15:0] Instruction_end;
-
-wire [31:0] MEM_mux_out;
-wire [31:0] Store_size_out;
-wire [31:0] Load_Size_out;
-
-wire [4:0] SA_mux_out;
-wire [31:0] IN_mux_out;
-wire [31:0] Shift_register_out;
-
-wire [4:0] WR_mux_out;
-wire [31:0] WD_mux_out;
-
-wire [31:0] Signal_extender_out;
-
-wire [31:0] Mult_msb_out;
-wire [31:0] Mult_lsb_out;
-
-wire [31:0] Div_remainder_out;
-wire [31:0] Div_quotient_out;
-
+    //Data from instruction
+    wire [5:0] OPCODE;
+    wire [4:0] RT;
+    wire [4:0] RS;
+    wire [15:0] OFFSET;
+//*************************************************************************************************************
+    wire [31:0] PC_out;
+    wire [31:0] ALUResult;
+    wire [31:0] ALUOut;
+    wire [31:0] Incond_j = {PC_out[31:28], RS,RT, OFFSET, 2'b00};
+    wire [31:0] EPC_out;
+    wire [31:0] REG1DATA_out;
+    wire [31:0] REG2DATA_out;
+    wire [31:0] regA_out;
+    wire [31:0] regB_out;
+    wire [31:0] MDR_out;
+    wire [31:0] SR_out;
+    wire [31:0] SE_out;
+    wire [31:0] SFL2_out;
+    wire [31:0] PC_MUX_out;
+    wire [31:0] MEM_MUX_out;
+    wire [4:0] WR_MUX_out;
+    wire [31:0] WD_MUX_out;
+    wire [4:0] SA_MUX_out;
+    wire [31:0] ULA_A_MUX_out;
+    wire [31:0] ULA_B_MUX_out;
 // Flag wires
-wire ula_overflow;
-wire ula_negative;
-wire ula_zero;
-wire ula_equals;
-wire ula_greaterthan;
-wire ula_lessthan;
-wire division_by_zero;
-
+    wire O;
+    wire N;
+    wire ZR;
+    wire EQ;
+    wire GT;
+    wire LT;
 // Instantiating modules
-
     //Muxes
-    hilo_mux HI_MUX (
-        HI_mux_control,
-        Div_remainder_out,
-        Mult_msb_out,
-        HI_mux_out
-    );
-
-    hilo_mux LO_MUX (
-        LO_mux_control,
-        Div_quotient_out,
-        Mult_lsb_out,
-        LO_mux_out
-    );
-
-    IN_mux IN_MUX (
-        in_mux_control,
-        B_Register_out,
-        A_Register_out,
-        Signal_extender_out,
-        IN_mux_out
-    );
-
-    mem_mux MEM_MUX (
-        mem_mux_control,
-        PC_out,
-        B_Register_out,
-        ALUout_output
-        MEM_mux_out
-    );
-
-    pc_mux PC_MUX (
-        pc_mux_control,
-        Load_Size_out,
-        ula_output,
-        ALUout_output,
-        Shift_left_2to28_out, // TODO fix inconditional jump // wire does not exist
+    pc_mux PC_MUX_(
+        PC_MUX,
+        ALUResult,//SUBSTITUIR DEPOIS POR LOAD SIZE
+        ALUResult,
+        ALUOut,
+        Incond_j,
         EPC_out,
-        A_Register_out,
-        PC_in
+        regA_out,
+        PC_MUX_out
     );
-
-    SA_mux SA_MUX (
-        sa_mux_control,
-        Instruction_end, // ???
-        B_Register_out,
-        MDR_out
+    mem_mux MEM_MUX_(
+        MEM_MUX,
+        PC_out,
+        regB_out,
+        ALUOut,
+        MEM_MUX_out
     );
-
-    ula_A_mux ULA_A_MUX (
-        ula_a_mux_control,
-        shift_left_2_out, // TODO fix offset branch // wire does not exist
-        A_Register_out,
-        Memory_data_out,
-        ula_operand_A
+    wr_mux WR_MUX_(
+        WR_MUX,
+        RT,
+        OFFSET,
+        WR_MUX_out
     );
-
-    ula_B_mux ULA_B_MUX (
-        ula_b_mux_control,
-        B_Register_out,
-        Signal_extender_out,
+    wd_mux WD_MUX_(
+        WD_MUX,
+        PC_out,
+        extend_1_32_out,
+        LO_out,
+        HI_out,
+        SR_out,
+        ALUOut,
+        ALUout,//Substituir depois pelo LOAD SIZE
+        WD_MUX_out
+    );
+    SA_mux SA_MUX_(
+        SA_MUX,
+        OFFSET,
+        regB_out,
+        MDR_out,
+        SA_MUX_out,
+    );
+    IN_mux IN_MUX_(
+        IN_MUX,
+        regB_out,
+        regA_out,
+        SE_out,
+        IN_MUX_out
+    );
+    ula_A_mux ULA_A_MUX_(
+        ULA_A_MUX,
+        SLF2_out,
+        regA_out,
+        Mem_out,
+        ULA_A_MUX_out
+    );
+    ula_B_mux ULA_B_MUX_(
+        ULA_B_MUX,
+        regB_out,
+        SE_out,
         PC_out,
         MDR_out,
-        ula_operand_B
+        ULA_B_MUX_out
     );
-
-    wd_mux WD_MUX (
-        wd_mux_control,
-        PC_out,
-        ext_to_32_out, // TODO fix less than flag // wire does not exist
-        LO_Register_out,
-        HI_Register_out,
-        Shift_register_out,
-        ALUout_output,
-        Load_Size_out,
-        WD_mux_out
-    );
-
-    wr_mux WR_MUX (
-        wr_mux_control,
-        RT,
-        Instruction_end, // ???
-        WR_mux_out
-    );
-
     //Registers
-    Registrador PC (
+    Registrador PC_(
         clk,
         reset,
-        PC_control,
-        PC_in,
+        PC_load,
+        PC_MUX_out,
         PC_out
     );
-
-    Registrador Memory_Data (
+    Memoria MEM_(
+        MEM_MUX_out,
         clk,
-        reset,
-        MDR_control,
-        Memory_data_out, // Gets output from memory
-        MDR_out
+        Mem_Write,
+        Mem_in,
+        Mem_out
     );
-
-    Registrador A (
+    Registrador MDR_(
         clk,
         reset,
-        A_control,
-        RegisterBank_out_A, // Gets output 1 from register bank
-        A_Register_out
+        MDR_load,
+        Mem_out,
+        MDR_out,
     );
-
-    Registrador B (
+    Instr_Reg IR_(
         clk,
         reset,
-        B_control,
-        RegisterBank_out_B, // Gets output 2 from register bank
-        B_Register_out
+        IR_write,
+        Mem_out,
+        OPCODE,
+        RS,
+        RT,
+        OFFSET
     );
-
-    Registrador HI (
+    Banco_reg REGS_(
         clk,
         reset,
-        HI_control,
-        HI_mux_out,
-        HI_Register_out
+        REGS_write,
+        RS,
+        RT,
+        WR_MUX_out,
+        WD_MUX_out,
+        REG1DATA_out,
+        REG2DATA_out
     );
-
-    Registrador LO (
-        clk,
-        reset,
-        LO_control,
-        LO_mux_out,
-        LO_Register_out
+    shiftleft_2 SFL2_(
+        SE_out,
+        SFL2_out
     );
-
-    Registrador ALUOut (
+    Registrador regA_(
         clk,
         reset,
-        ALUout_control,
-        ula_output, // Gets operation result from ALU
-        ALUout_output
+        regA_load,
+        REG1DATA_out,
+        regA_out
     );
-
-    Registrador EPC (
+    Registrador regA_(
         clk,
         reset,
-        EPC_control,
-        ALUout_output,
+        regB_load,
+        REG2DATA_out,
+        regB_out
+    );
+    ula32 ALU_(
+        ALU_A_MUX_out,
+        ALU_B_MUX_out,
+        ALUOp,
+        ALUResult,
+        O,
+        N,
+        ZR,
+        EQ,
+        GT,
+        LT
+    );
+    Registrador ALUOut_(
+        clk,
+        reset,
+        ALUOut_load,
+        ALUResult,
+        ALUOut
+    );
+    Registrador EPC_(
+        clk,
+        reset,
+        EPC_load,
+        ALUOut,
         EPC_out
     );
-
+    extend_32 extend_1_32_(
+        LT,
+        extend_1_32_out
+    );
+    RegDesloc SR_(
+        clk,
+        reset,
+        Shift,
+        SA_MUX_out,
+        IN_MUX_out,
+        SR_out
+    );
+    sign_extend SE_(
+        OFFSET,
+        SE_out
+    );
+    control_unit CONTROL_UNIT_(
+        clk,
+        reset,
+        O,
+        N,
+        ZR,
+        EQ,
+        GT,
+        LT,
+        OPCODE,
+        OFFSET[5:0],
+        PC_load,
+        MDR_load,
+        regA_load,
+        regB_load,
+        EPC_load,
+        ALUOut_load,
+        Mem_write,
+        IR_write,
+        REGS_write,
+        ALUOp,
+        Shift,
+        PC_MUX,
+        MEM_MUX,
+        WR_MUX,
+        WD_MUX,
+        SA_MUX,
+        IN_MUX,
+        ULA_A_MUX,
+        ULA_B_MUX,
+    );
     //Provided modules
-    Banco_reg Registers (
-        clk,
-        reset,
-        Register_bank_control,
-        RS,
-        RT,
-        WR_mux_out,
-        WD_mux_out,
-        RegisterBank_out_A,
-        RegisterBank_out_B
-    );
-
-    Instr_Reg Instruction_Register (
-        clk,
-        reset,
-        Instruct_Reg_control,
-        Memory_data_out,
-        OPcode,
-        RS,
-        RT,
-        Instruction_end
-    );
-
-    Memoria Memory (
-        MEM_mux_out,
-        clk,
-        MEM_control,
-        Store_size_out,
-        Memory_data_out
-    );
-
-    RegDesloc Shift_register (
-        clk,
-        reset,
-        Shift_register_control,
-        SA_mux_out,
-        IN_mux_out,
-        Shift_register_out
-    );
-
-    ula32 ULA (
-        ula_operand_A,
-        ula_operand_B,
-        ula_control,
-        ula_output,
-        ula_overflow,
-        ula_negative,
-        ula_zero,
-        ula_equals,
-        ula_greaterthan,
-        ula_lessthan
-    );
 
     //Miscellaneous modules
-    div Div (
-        clk,
-        reset,
-    );
-
-    mult Mult (
-        clk,
-        reset,
-    );
-
-    extend_32 Xtnd32 (
-
-    );
-
-    load_size Load_size (
-
-    );
-
-    store_size Store_size (
-
-    );
-
-    shiftleft_2 Shiftleft2 (
-
-    );
-
-    shiftleft_2to28 Shifteleft_2to28 (
-
-    );
-
-    sign_extend Sign_xtnd (
-
-    );
 
     //Control unit
-    control_unit Control_Unit (
-        clk,
-        reset,
-        //instruction
-        OPcode,
-        Instruction_end, // should be funct only
-        //flags
-        ula_overflow,
-        ula_negative,
-        ula_zero,
-        ula_equals,
-        ula_greaterthan,
-        ula_lessthan,
-        division_by_zero,
-        //mux controls
-        HI_mux_control,
-        LO_mux_control,
-        in_mux_control,
-        mem_mux_control,
-        pc_mux_control,
-        sa_mux_control,
-        ula_a_mux_control,
-        ula_b_mux_control,
-        wd_mux_control,
-        wr_mux_control,
-        //register controls
-        PC_control,
-        MDR_control,
-        A_control,
-        B_control,
-        HI_control,
-        LO_control
-        ALUout_control,
-        EPC_control,
-        //functional controls
-        ula_control,
-        Register_bank_control,
-        Instruct_Reg_control,
-        MEM_control,
-        Shift_register_control,
-    );
 
 endmodule
